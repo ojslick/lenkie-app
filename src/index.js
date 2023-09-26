580289a45bd9f8b5294e16979b606cf0a58a0031
Oluwaseun Akogun <42413239+ojslick@users.noreply.github.com>
2023-09-26 23:24:13 +0100

    chore: integrate centry

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+    dsn: 'https://6af28235b12f13bbb37eafcc44441a8f@o4505948904947712.ingest.sentry.io/4505948907110405',
+    integrations: [
+        new Sentry.BrowserTracing({
+            tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+        }),
+        new Sentry.Replay(),
+    ],
+
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
